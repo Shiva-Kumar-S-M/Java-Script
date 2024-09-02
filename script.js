59897655988c48172004e1c8dc3cb2434fd0e702
edwin <shivukumar.iu@gmail.com>
2024-09-02 18:46:16 +0200

    Notes

---
+++ script.js
@@ -0,0 +1,8 @@
+// 1) words and Keywords
+// 2) variables let constants variable can be changed but not constants 
+// 3) Hoisting declaring before initializing.
+// 4) Conditional statements if else else if
+// 5)  types primitive & reference [],{},()
+// 6) while loops
+// 7) function -- parameters & arguments 
+// 8)arrays -- push pop shift &unshift
