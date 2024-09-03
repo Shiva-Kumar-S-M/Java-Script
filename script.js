2b5b46fc634584f9b43a27cf93bd2096e5b97e08
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-09-03 18:30:39 +0530

    Key

--- script.js
+++ script.js
@@ -7,3 +7,33 @@
 // 7) function -- parameters & arguments 
 // 8)arrays -- push pop shift &unshift
 // hello how are you
+
+
+
+
+
+
+// advance 
+// diff b/w var & let const
+// 1) es 5 & es6
+// 2) function scoped & braces scoped
+// 3) add itself to window object & where as it doesn't
+
+//execution context -- variables , funciton ,lexical environment
+
+
+
+let x ;
+for (x = 0 ; x<10 ;x++){
+    console.log(x);
+}
+
+
+//1) how to copy references values
+//2) truthy & falsy
+//3)switch
+//4)for-each , for-in , for-of,do-while
+//5) what is first class funcs 
+// 6) how are arrays are made behind the scenes 
+//7) why we can make negative indexes arrays 
+// 8) how to deleate prop
