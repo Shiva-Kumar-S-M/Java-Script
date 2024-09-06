6c84931cac27c7f3b1360401785a946cd202c1ee
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-09-06 19:19:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,272 +1,120 @@
-// 1) words and Keywords
-// 2) variables let constants variable can be changed but not constants 
-// 3) Hoisting declaring before initializing.
-// 4) Conditional statements if else else if
-// 5)  types primitive & reference [],{},()
-// 6) while loops
-// 7) function -- parameters & arguments 
-// 8)arrays -- push pop shift &unshift
-// hello how are you
+// // // for loop
+// // for (let count=1; count <= 5 ; count++){
 
+// //     console.log("Shivu");
+// // }
+// // console.log("loop has ended");
 
+// // calculate sum of n number
+// let sum = 0 ;
+// for ( let i =1; i <=10 ; i++
+// ){
+//     sum = sum +i;
+// }
+// console.log("Sum is :" , sum);
+  
+// while loops
+// while(condition){
+//     // do same work
+// }
+// let i = 1
+// while( i <= 5){
+//     console.log("Hey good morning");
+//     i++;
+// }
 
+// do while loop
+// let i = 1;
+// do {
+//     console.log("Hello!");
+// } while( i <=10);
+// // do for loop
 
+// for of loop
+// let str = "shivu";
+// for (let val of str){
+//     console.log("val:",val);
+// }
 
+// for in loop
+//  let student = {
+//     Name : "shivu",
+//     age : 18,
+//     cgpa : 8.8 ,
 
-// advance 
-// diff b/w var & let const
-// 1) es 5 & es6
-// 2) function scoped & braces scoped
-// 3) add itself to window object & where as it doesn't
-
-//execution context -- variables , funciton ,lexical environment
-
-
-
-// let x ;
-// for (x = 0 ; x<10 ;x++){
-//     console.log(x);
-
-
-
-//1) how to copy references values
-//2) truthy & falsy
-//3)switch
-//4)for-each , for-in , for-of,do-while
-//5) what is first class funcs 
-// 6) how are arrays are made behind the scenes 
-//7) why we can make negative indexes arrays 
-// 8) how to deleate prop
-
-
-
-//-----------------------practise -------------------------//
-
-// if else loop
-// let  mode = "dark";
-// let color;
-//  if (mode === "dark"){
-//     color = "black";
-
-//  } else {
-//     color = "white";
+//  } ;
+//  for ( let key in student){
+//     console.log(key ,"=" ,student[key]);
 //  }
-//  console.log(color);
 
 
-//else if statements 
-
-// let age = prompt("enter your age");
-// if (age === 18){
-//     console.log("you are teen ager");
-// }
-// else if (age>18){
-//     console.log("You are senoir");
-// }
-// else {
-//     console.log("You are kidoo");
+// print even numbers
+// for ( let num =0 ; num <=100 ; num++){
+//     if (num%2 ===0) {
+//         console.log( "num is =",num);
+//     }
 // }
 
-
-// Ternary operator 
-
-// condition ? true output : false output;
-
-
-// let age = 17;
-//  age === 17? console.log("Adult"): console.log("Kidoo");
-
-// --------- FUNCTIONS ------------//
-// syntax : function functionName(){
-//   // do dome work 
-
-// } functionName();
-
-
-// function fun(){
-//     console.log("Today collage is holiday on account of sunday");
+// practise 2 game
+// let gameNum = 25 ;
+// let userNum = prompt("Guess the number:");
+// while (userNum != gameNum){
+//     userNum = prompt("you enterd wrong number. Guess again :");
 // }
-// fun();
+// console.log("congratulatons you entered the right number");
+
+// let str = "Apna collage";
+// console.log(str[5]);
+
+// // template literals
+// let specialString = `this is literal statement`;
+// console.log(specialString);
 
 
-// function sum (x,y){                   // parameters input
-//     console.log(x+y);
-// }
-// sum (200,300);                        // arguments
+//  let obj = {
+//      item : "Pen",
+//      price : 20,
+//  }
+//  let output = `the cost of ${obj.item} is ${obj.price}`;
+//  console.log(output);
 
 
-// function importent(msg){
-//     console.log(msg);
-//     importent("I am going to get myself back with strong mindset");}
+// /* string interpolation 
+// to create string by doing substituion of palceholders */
 
+// console.log("Shivu \n Kumar");
 
-
-// function sum (x,y){
-//     s = x+y;
-//     return s;
-// }
-
-// let val = sum(250,3500);
-// console.log(val);
-
-// // function parameter are like local variable of function -- block scope
-
-// -------- arrow function --------//
-
-// const arrowSum = (a,b)=>{
-//     console.log(a+b);
-// };
-// arrowSum(20,50); 
-
-
-
-// const arrowMulti = (a,b)=> {
-//     console.log(a*b);
-// };
-
-// arrowMulti(55,55);
-
-// const square = (a,b)=>{
-//     console.log(a**b);
-
-// };
-// square(9,2);
-
-// -----for Each loops --------//
-
-// let abc =[1,2,3,4,5];
-// abc.forEach((val) =>{
-//     console.log(val);
-// });
-
-
-// let arr = [4,5,6];
-// arr.forEach((val)=>
-// {
-//     console.log(val**2);
-// });
-
-// // ----------- Mapping -------------//
-// let num =[10,20,30,40,50];
-// let newArray = num.map((val)=>
-// {
-//     return val;
-// });
-// console.log(newArray);
-
-
-// let old = [100,200,300,400,500,600,700];
-// let newsh = old.map((val)=>
-// {
-//     return val;
-// });
-
-// console.log(newsh);
-
-
-// --------filtering --------//  new array of elements that give true for condition/filter
-// let arr =[10,11,12,13,45,47,89];
-// let newarray = arr.filter((val)=>{
-//     return val %2 === 0;
-// });
-// console.log(newarray);
-
-
-
-// let number = [124,568,41,4563,153,524,124,515];
-// let even = number.filter((val)=>{
-//     return val %2 === 0;
-// });
-// console.log(even);
-
-// let n = prompt("Enter number of array");
-// let arr =[];
-// for (let i =0;i<n;i++){
-//     arr[i-1]=i;
-// }
-// console.log(arr);
-
-// let sum = arr.reduce((res,curr)=>{
-//     return res+curr;
-// });
-// console.log(sum);
-
-
-// let n = prompt("Enter number of Array elements");
-// let arr =[];
-// for(i=0;i<=n;i++){
-//     arr[i-1]=i;
-// }
-// console.log(arr);  
-// let prod = arr.reduce((res,curr)=>{
-//     return res*curr;
-// });
-// console.log(prod);
-
-
-// let a = prompt("Enter a number of array elemnts");
-// let arr=[];
-// for (i=1;i<=a;i++){
-//     arr[i]=i;
-// };
-// console.log(arr);
-//  let sum= arr.reduce((res,curr)=>{
-//     return res+curr;
-//  });
-//  console.log(sum);
-//  let product = arr.reduce((res,curr)=>{
-//     return res*curr;
-//  });
-//  console.log(product);
-
-
-// DOM OBJECT 
-// let heading = document.getElementById("heading");
-// console.log(heading);
-
-
-// let elements = document.querySelectorAll(".dom");
-// console.dir(elements);
-
-// alert("how are you")
-// console.dir(document.body.childNodes[1]);
-// console.log(document.body);
-
-// document.body.style.background="Yellow";
-
-//-----------DOM manipulation------------//
-//selecting with id
-// let heading = document.getElementById("heading");
-// console.log(heading);
-// console.dir(heading);
-
-// selecting with class
-// let same = document.getElementsByClassName("dom");
-// console.log(same);
-// console.dir(same);
+/* String methods */
+// let str = "shivukumar";
+// newStr = str.toUpperCase();
+// console.log(newStr);
  
-// selecting with tag
-// let para =document.getElementsByTagName("p");
-// console.log(para);
-// console.dir(para)
+// let val = "    have a nice day   ";
+// console.log(val.trim());
 
 
-//query selector 
+// let num = "This is even number";
+// console.log(num.toUpperCase());
 
-// let firstel = document.querySelector(".dom");  //first element
-// console.dir(firstel);
+// sliceing str.slice(start ,end?) return part of stirng 
+// let str =  "shivu kumar";
+// console.log(str.slice());
 
-
-// let allel = document.querySelectorAll(".dom");
-// console.dir(allel);
-
-// // properties 
-// 1) tagName = return tag for element NodeList
-// 2) innerText = return text content of elements and all its children
-// 3) innerHtml = returns the plain text or html contents in the element
-// 4)
+// // string concatination
+// let str1 ="Sharan";
+// let str2 = "Cherry";
+// console.log(str1.concat(str2));
 
 
+// sting replace
+// let name = "Shivu";
+// console.log(name.replace("Shivu", "Sharan"));
 
+//string character
+// let length = "0012345";
+// console.log(length.charAt(5));
 
-// document.querySelector("div").children 
+// practise
+let fullName = prompt("Enter your Name:");
+let userName = "@" + fullName + fullName.length;
+console.log(userName);
