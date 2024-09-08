1f44ad1b2b58041c21051aff479c1f3e5b2bdf9f
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-09-08 08:43:40 +0530

    push

---
+++ DOM/script.js
@@ -0,0 +1,12 @@
+// let fruits = document.querySelector("div");   
+// console.dir(fruits);        // fruits.innerText 
+//                             // fruits.innerHTML
+// let heading = document.querySelector("h1");  //heading.innerText  //heading.textConetent
+
+
+
+// let heading = document.querySelector("h3");
+// console.dir(heading.innerText);
+// heading.innerText = heading.innerText + " from apna collage students";    // concatinantion
+ let div = document.querySelectorAll(".box");
+ console.log(div);
