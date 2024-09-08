94c5fac64bf24c4b41b2bac27b75639cdbe9f89e
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2024-09-08 09:17:48 +0530

    Ass

--- DOM/script.js
+++ DOM/script.js
@@ -8,5 +8,13 @@
 // let heading = document.querySelector("h3");
 // console.dir(heading.innerText);
 // heading.innerText = heading.innerText + " from apna collage students";    // concatinantion
- let div = document.querySelectorAll(".box");
- console.log(div);
+ let box = document.querySelectorAll(".box");
+ let idx =1;
+for (div of box){
+    div.innerText = `new unique value ${idx}`;
+    idx++;
+}
+//  console.log(div);
+//  div[0].innerText = "New Value 1";
+//  div[1].innerText = "new value 2";
+//  div[2].innerText = "new value 3";
